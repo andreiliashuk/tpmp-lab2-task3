4ccb93d1e4271847c5546b923b8b2e4371eab8e2
Andrei <andreylyashuk147@gmail.com>
2026-03-13 10:05:41 +0300

    Update znak.h

--- src/znak.h
+++ src/znak.h
@@ -4,23 +4,53 @@
 #ifndef ZNAK_H
 #define ZNAK_H
 typedef struct {
-    int year;
-    int month;
-    int day;
+    int year;   /* Year (e.g., 1990) */
+    int month;  /* Month (1-12) */
+    int day;    /* Day (1-31) */
 } Date;
 
+/*
+ * ZNAK structure containing personal information and zodiac sign.
+ */
 typedef struct {
-    char name[50];
-    char zodiac[20];
-    Date birth_date;
+    char name[50];        /* Full name (surname and first name) */
+    char zodiac[20];      /* Zodiac sign */
+    Date birth_date;      /* Birth date */
 } ZNAK;
 
-
+/*
+ * Function: input_znak_array
+ *   Reads data from keyboard into an array of ZNAK structures.
+ *   arr: array of ZNAK structures
+ *   size: maximum number of elements to read
+ *   returns: number of successfully read elements
+ */
 int input_znak_array(ZNAK arr[], int size);
 
-void sort_by_date(ZNAK arr[], int  n);
+/*
+ * Function: sort_by_date
+ *   Sorts ZNAK array by birth date in ascending order.
+ *
+ *   arr: array to sort
+ *   n: number of elements in the array
+ */
+void sort_by_date(ZNAK arr[], int n);
+
+/*
+ * Function: print_by_zodiac
+ *   Prints information about people with given zodiac sign.
+ *   arr: array of ZNAK structures
+ *   n: number of elements in the array
+ *   zodiac: zodiac sign to search for
+ *   returns: number of found people
+ */
 int print_by_zodiac(const ZNAK arr[], int n, const char zodiac[]);
 
+/*
+ * Function: print_znak
+ *   Prints a single ZNAK structure.
+ *   z: structure to print
+ */
 void print_znak(const ZNAK z);
 
-#endif 
+#endif /* ZNAK_H */
